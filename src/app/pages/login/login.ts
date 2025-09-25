a1692e5ac819850aab552ee9975d689d51ac84fa
samuelmota321 <samuel.mota.2010@gmail.com>
2025-09-25 15:53:39 -0300

    login service

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,40 +1,46 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-
 import { AuthLayout } from '../../components/auth-layout/auth-layout';
+import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [
-    CommonModule,
-    FormsModule,
-    AuthLayout,
-    RouterLink
-  ],
+  standalone: true,
+  imports: [AuthLayout, FormsModule, CommonModule, RouterLink],
   templateUrl: './login.html',
-  styleUrls: ['./login.scss']
+  styleUrl: './login.scss'
 })
 export class Login {
   credentials = {
     email: '',
     password: ''
   };
+
   errorMessage: string | null = null;
+  submitted = false;
 
   constructor(private authService: AuthService, private router: Router) {}
 
   onSubmit() {
+    this.submitted = true; 
     this.errorMessage = null;
+
+    if (!this.credentials.email || !this.credentials.password) {
+      this.errorMessage = 'Por favor, preencha o e-mail e a senha corretamente.';
+      return; 
+    }
+
     this.authService.login(this.credentials).subscribe({
       next: () => {
-        console.log('Login bem-sucedido!');
       },
       error: (err) => {
-        this.errorMessage = 'Email ou senha inválidos. Por favor, tente novamente.';
-        console.error('Erro no login', err);
+        if (err.status === 409) {
+          this.errorMessage = 'Email ou senha incorretos.';
+        } else {
+          this.errorMessage = 'Ocorreu um erro inesperado. Tente novamente mais tarde.';
+        }
       }
     });
   }
