e213fc10b8dcf81ebbd6ce965c67df35b2fe185e
samuelmota321 <samuel.mota.2010@gmail.com>
2025-10-09 14:45:50 -0300

    responsive styles

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -15,6 +15,10 @@ export class Header {
   isLoggedIn$: Observable<boolean>;
   isAdmin$: Observable<boolean>;
 
+  isHidden = false;
+  isMenuOpen = false;
+  private lastScrollY = 0;
+
   constructor(private authService: AuthService, private router: Router) {
     this.isLoggedIn$ = this.authService.isLoggedIn$;
     this.isAdmin$ = this.authService.isAdmin$;
@@ -22,6 +26,24 @@ export class Header {
 
   logout() {
     this.authService.logout();
+    this.isMenuOpen = false; // Fecha o menu ao deslogar
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY > this.lastScrollY && currentScrollY > 60) {
+      this.isHidden = true;
+      this.isMenuOpen = false; // Fecha o menu ao rolar
+    } else {
+      this.isHidden = false;
+    }
+
+    this.lastScrollY = currentScrollY;
   }
 }
-
